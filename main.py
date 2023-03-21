ea9e1081b2c0847a69406f08d424732516a92e84
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2023-03-21 15:05:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import postfix
-from direct_dfa import DFA
+from direct_dfa import DirectDFA
 from syntaxTree import SyntaxTree
 from thompson import Thompson
 from postfix import *
-from nfa_dfa import NFAtoDFA
+from nfa_dfa import DFA
 
 # Direct DFA
 # ---------------------------------------------------------
@@ -14,7 +14,7 @@ canonicalER = '(' + regex + ')' + '#'
 tree = SyntaxTree(operators, canonicalER, direct=True)
 pts = tree.positions(tree.raiz, lleno=True)
 # DFA Ops
-dfa = DFA(syntaxTree=tree, direct=True, circs=pts)
+dfa = DirectDFA(syntaxTree=tree, direct=True, circs=pts)
 # Create DFA in memory
 dfa.buildGraph()
 # Show DFA
@@ -30,8 +30,8 @@ print("Postfix value: " + ptf)
 new_nfa = Thompson(ptf)
 new_nfa.compile()
 # Create new DFA
-new_dfa = NFAtoDFA(new_nfa.startingState, new_nfa.finalState,
-                   new_nfa.states, new_nfa.symbolList, new_nfa.splitTransitionsList)
+new_dfa = DFA(new_nfa.startingState, new_nfa.finalState,
+              new_nfa.states, new_nfa.symbolList, new_nfa.splitTransitionsList)
 new_dfa.compile()
 # NFA simulation
 char_string = input("\nString to be evaluated in NFA:\n")
